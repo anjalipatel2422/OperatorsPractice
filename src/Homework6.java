3f68bb12d2893007988e8404ecbc5f456f06582e
anjalipatel2422 <anjalipatel2422@gmail.com>
2020-06-10 00:59:49 +0530

    'homeworkoperator'

---
+++ src/Homework6.java
@@ -0,0 +1,16 @@
+public class Homework6 {
+    static int a = 10; // global variable
+   static int b =5; // global variable
+   static int c =20;
+   //static int d=60;
+
+
+
+
+    public static void main(String[]args){//java main method
+        System.out.println(a>b||b<c);
+        System.out.println(a+b==c-b);
+
+
+    }
+}
